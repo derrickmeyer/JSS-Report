b0b3999e4a40ddbb51422746b0b1a406d12f5bf4
Jeremy Matthews <jeremymatthews@mac.com>
2013-10-16 11:01:56 -0500

    - constants file for variable declaration

---
+++ JSS Report/SWJMConstants.h
@@ -0,0 +1,20 @@
+//
+//  SWJMConstants.h
+//  JSS Report
+//
+//  Created by Jeremy Matthews on 9/24/13.
+//  Copyright (c) 2013 SISU Works LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWJMConstants : NSObject
+{
+    
+}
+
+extern NSString *const hostname;
+extern NSUInteger const port;
+extern BOOL usesSSL;
+
+@end
---
+++ JSS Report/SWJMConstants.m
@@ -0,0 +1,21 @@
+//
+//  SWJMConstants.m
+//  JSS Report
+//
+//  Created by Jeremy Matthews on 9/24/13.
+//  Copyright (c) 2013 SISU Works LLC. All rights reserved.
+//
+
+#import "SWJMConstants.h"
+
+@implementation SWJMConstants
+{
+    
+}
+
+NSString *const hostname = @"www.myjss.com";
+NSUInteger const port = 8443;
+BOOL usesSSL = YES;
+
+@end
+
